cc4953586e6355d56db7a34b4f8c84ad38968ac0
lxvrns <personal@laurensmorales.dev>
2023-04-13 16:33:56 +1200

    Complete match and get token procs

--- algorithms-and-languages/assignment-1/src/main.nim
+++ algorithms-and-languages/assignment-1/src/main.nim
@@ -1,4 +1,4 @@
-import re, tables, tokenize
+import re, tokenize
 
 echo """
 ----------------------------------------
@@ -13,31 +13,50 @@ type
     tokenType: string
     value: string
 
-let tokenPatterns: OrderedTable[string, Regex] = {
-  "literal": re("\"[[:ascii:]]*\""),
-  "append": re"append",
-  "exit": re"exit",
-  "list": re"list",
-  "print": re"print",
-  "printlength": re"printlength",
-  "printwords": re"printwords",
-  "printwordcount": re"printwordcount",
-  "reverse": re"reverse",
-  "set": re"set",
-  "constant": re"\s", #
-  "end": re";",
-  "plus": re"\+",
-  "id": re"[a-zA-Z][a-zA-Z0-9_]",
-}.toOrderedTable()
+
+
+
+func matchToken(token: string): Token =
+  if token == "": return (tokenType: "", value: "")
+
+  const TOKENPATTERNS = [ # evaluated at compile time
+    ("literal", "\\\"[[:ascii:]]*\\\""),
+    ("append", "append"),
+    ("exit", "exit"),
+    ("list", "list"),
+    ("print", "print"),
+    ("printlength", "printlength"),
+    ("printwords", "printwords"),
+    ("printwordcount", "rintwordcount"),
+    ("reverse", "reverse"),
+    ("set", "set"),
+    ("constant", "\\s"),
+    ("end", ";"),
+    ("plus", "\\+"),
+    ("id", "[a-zA-Z]+[a-zA-Z0-9_]*"),
+  ]
+  for tokenType in TOKENPATTERNS:
+    if match(token, re(tokenType[1])):
+      return (tokentype[0], token)
 
 proc getTokens(str: string): seq[Token] =
-  for token in tokenize(str): # 'tokenize' returns: [typeOfToken, isSeparator]
-    for tokenType, regexPattern in tokenPatterns:
-      if match(token[0], regexPattern):
-        result.add((tokenType, token[0])) # result is declared & 
-        break                             # returned implicitly
+  for token in str.tokenize(): # 'tokenize' returns: [string, isSeparator]
+    if matchToken(token[0]).tokenType == "":
+      raise newException(ValueError, "Invalid token: \"" & token[0] & "\"")
+    else:
+      result.add(matchToken(token[0]))
     
+proc handleInput() =
+  let userIn: string = readLine(stdin)
+  var tokens: seq[Token]
+  try:
+    tokens = getTokens(userIn)
+    if tokens.len == 0: return # ignore empty imput
+  except ValueError:
+    echo getCurrentExceptionMsg()
+    echo "Try again."
+    return
+  echo tokens
 
 while true:
-  let userIn: string = readLine(stdin)
-  discard getTokens(userIn)
+  handleInput()
